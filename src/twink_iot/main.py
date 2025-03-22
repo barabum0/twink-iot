8aabd170ee139a1a932f6e2304b5692d9a374a0b
sushka <r.m.poltorabatko@gmail.com>
2025-03-22 16:42:15 +0300

    fix methods

--- src/twink_iot/main.py
+++ src/twink_iot/main.py
@@ -1,6 +1,6 @@
 import os
 
-from bottle import get, run
+from bottle import get, post, run
 
 from twink_iot.twink import GyverTwinkUDP
 
@@ -10,13 +10,13 @@ twink = GyverTwinkUDP(
 )
 
 
-@get("/gyvertwink/set-pwr/<pwr:int>")
+@post("/gyvertwink/set-pwr/<pwr:int>")
 def set_power(pwr: int) -> str:
     twink.set_power(bool(pwr))
     return "OK"
 
 
-@get("/gyvertwink/set-brightness/<brightness:int>")
+@post("/gyvertwink/set-brightness/<brightness:int>")
 def set_brightness(brightness: int) -> str:
     twink.set_brightness(int(brightness * 2.55))
     return "OK"
